0371bd04b23c73d178927266fb70d77a1056d387
Aaron Garber-Paul <aaron@MacBook-Pro-2.local>
2021-01-28 00:44:16 -0800

    last polish

--- D3_data_journalism/D3_Archieved_attempts/appBEST2.js
+++ D3_data_journalism/D3_Archieved_attempts/appBEST2.js
@@ -1,16 +1,16 @@
-//MY CODE HERE!
-
+// @TODO: YOUR CODE HERE!
 var svgArea = d3.select("body").select("svg");
 
 // SVG wrapper dimensions are determined by the current width and
 // height of the browser window.
 var svgWidth = window.innerWidth;
 var svgHeight = window.innerHeight;
+
 var margin = {
   top: 50,
   bottom: 50,
   right: 50,
-  left: 100
+  left: 50
 };
 
 var height = svgHeight - margin.top - margin.bottom;
@@ -29,75 +29,53 @@ var chartGroup = svg.append("g")
 
 // Read CSV
 d3.csv("assets/data/data.csv").then(function(censusData) {
-  console.log(censusData);
-
-  // create date parser
-  var dateParser = d3.timeParse("%d-%b");
-
+    console.log(censusData);
+ 
   // parse data
   censusData.forEach(function(data) {
     data.obesity =  +data.obesity;
-    data.income = +data.income;;
+    data.income = +data.income;
   });
 
+
   // create scales
-  var xLinearScale = d3.scaleLinear()
-    .domain(d3.extent(censusData, d => (d.obesity-.5)))
-    .range([0, (width)]);
+
   var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(censusData, d => (d.income+8000))])
+    .domain([0, d3.max(censusData, d => d.income)])
     .range([height, 0]);
+
+  var xLinearScale = d3.scaleLinear()
+    .domain([15, d3.max(censusData, d => d.obesity)])
+    .range([0, width]);
 
   // create axes
   var xAxis = d3.axisBottom(xLinearScale);
-  var yAxis = d3.axisLeft(yLinearScale).ticks(6);
+  var yAxis = d3.axisLeft(yLinearScale);
 
   // append axes
   chartGroup.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(xAxis);
+
   chartGroup.append("g")
     .call(yAxis);
 
-  // Add Y axis label:
-  chartGroup.append("text")
-      .attr("text-anchor", "end")
-      .attr("transform", "rotate(-90)")
-      .attr("x", 0 - (margin.left))
-      .attr("y", -50)
-      .text("Average Income ($)");
-  
   // Add X axis label:
-  chartGroup.append("text")
+  svg.append("text")
       .attr("text-anchor", "end")
-      .attr("x", (width/2))
-      .attr("y", (height +30))
-      .text("Obesity Rate (%)")
-      .attr("text-anchor", "start");
+      .attr("x", width)
+      .attr("y", height+50 )
+      .text("Average Obesity");
+  
+      // Add Y axis label:
+  svg.append("text")
+      .attr("text-anchor", "end")
+      .attr("x", 0)
+      .attr("y", height/2)
+      .text("Average Income")
+      .attr("text-anchor", "start")
 
-   // Add a title
-  chartGroup.append("text")
-      .attr("x", (width / 2))             
-      .attr("y", 0 - (margin.top / 2))
-      .attr("text-anchor-title", "left")  
-      .style("font-size", "20px") 
-      .style("text-decoration", "underline")  
-      .text("Average Income vs. Obesity Rate (by state)");
 
-  // Append Abbreviation to each data point
-  chartGroup.append("text")
-  .selectAll("circles")
-  .data(censusData)
-  .enter()
-  .append("tspan")
-      .attr("x", d => xLinearScale(d.obesity))
-      .attr("y", d => yLinearScale(d.income))
-      .attr("text-anchor", "middle")
-      .text(function(data) {
-          return data.abbr
-      })
-      .attr("fill", "blue")
-      .attr("font-size", 10, "bold");
 
   // append circles
   var circlesGroup = chartGroup.selectAll("circle")
@@ -106,34 +84,34 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
     .append("circle")
     .attr("cx", d => xLinearScale(d.obesity))
     .attr("cy", d => yLinearScale(d.income))
-    .attr("r", "10")
+    .attr("r", "20")
     .attr("fill", "gold")
-      .attr("fill-opacity","0.3")
-    .attr("stroke", "black")
-      .attr("stroke-width", "1");
-
-
-
+    .attr("stroke-width", "1")
+    .attr("stroke", "black");
+  
 
   // Step 1: Append tooltip div
-  var toolTip = d3.select("body")
-    .append("div")
+  var toolTip = d3.select("chartBody")
+    .append("svg")
     .classed("tooltip", true);
 
   // Step 2: Create "mouseover" event listener to display tooltip
   circlesGroup.on("mouseover", function(d) {
-    toolTip.style("display", "block")
+      console.log(d.abbr)
+      toolTip.style("display", "block")
         .html(
-          `<strong>${d.state}<strong><hr>Obesity rate: ${d.obesity}%<hr> Avg Annual Income: $${d.income}.00`)
+          `<strong>(d.state)<strong><hr>Obesity rate: (d.obesity)<hr> Average Annual Income: (d.income)`
+        )
         .style("left", d3.event.pageX + "px")
         .style("top", d3.event.pageY + "px");
   })
-
   // Step 3: Create "mouseout" event listener to hide tooltip
-  .on("mouseout", function() {
+  .on("mouseout", function(d) {
     toolTip.style("display", "none");
   });
+
 }
 , function(error) {
   console.log(error);
 });
+
